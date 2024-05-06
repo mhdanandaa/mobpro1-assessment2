a8c1d7d8b843c78b7d4d4fe0c134c755377e0ff0
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2024-05-06 20:24:23 +0700

    Menambahkan Tombol FAB

--- app/src/main/java/org/d3if3066/efwangarage/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3066/efwangarage/ui/screen/MainScreen.kt
@@ -1,13 +1,11 @@
 package org.d3if3066.efwangarage.ui.screen
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,10 +17,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.CoroutineScope
 import org.d3if3066.efwangarage.R
 import org.d3if3066.efwangarage.ui.theme.EfwanGarageTheme
-
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,6 +36,17 @@ fun MainScreen(navController: NavHostController) {
                     titleContentColor = MaterialTheme.colorScheme.primary
                 )
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.tambah_mobil),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
         }
     ) { padding ->
         ScreenContent(Modifier.padding(padding))
